138c231a455ad5ffb14479e0f9aa507112594d50
Weerayut Teja <teja.weerayut@gmail.com>
2021-09-10 10:55:42 +0700

    Add palindrome problem

---
+++ algorithms/javascript/9-palindrome-number/solution-1.js
@@ -0,0 +1,8 @@
+// Personally I think this one might be nonesense for JS Dev. Because of numbers of string methods to use. Look like cheat, but just a choice.
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+ var isPalindrome = function(x) {
+    return x.toString().split('').reverse().join('') === x.toString();
+};
