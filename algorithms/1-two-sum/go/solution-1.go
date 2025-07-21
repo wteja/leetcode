560493aa89e145da31f4935fe33dcc49168a6c32
Weerayut Teja <weerayut.te@itthirit.io>
2025-07-22 03:14:58 +0700

    Refactor twoSum solution to use main package and add example usage

--- algorithms/1-two-sum/go/solution-1.go
+++ algorithms/1-two-sum/go/solution-1.go
@@ -1,4 +1,6 @@
-package problem1
+package main
+
+import "fmt"
 
 func twoSum(nums []int, target int) []int {
 	pair := []int{}
@@ -14,4 +16,10 @@ func twoSum(nums []int, target int) []int {
 		}
 	}
 	return pair
+}
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9)) // Output: [0, 1]
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))      // Output: [1, 2]
+	fmt.Println(twoSum([]int{3, 3}, 6))         // Output: [0, 1]
 }
